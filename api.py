eeb46a3db2d2def6b7b8b8b4b0c59616a01649a9
Jesse Jaggars <jjaggars@redhat.com>
2019-04-08 14:33:51 -0500

    get/ tracer bullet
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- api.py
+++ api.py
@@ -4,7 +4,11 @@ from db import Preference
 
 def get():
     res = Preference.query.filter((Preference.account == user["account"])).one_or_none()
-    return res.prefs if res else None, 200
+    doc = {
+        "version": "v0",
+        "prefs": res.prefs if res else None
+    }
+    return doc, 200
 
 
 def post(request):
