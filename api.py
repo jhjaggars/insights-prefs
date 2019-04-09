e3e25c1281d6a7668d33b4718bcd104144bdac96
Jesse Jaggars <jjaggars@redhat.com>
2019-04-09 07:59:06 -0400

    save and add working
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- api.py
+++ api.py
@@ -2,13 +2,26 @@ from app import user
 from db import Preference
 
 
+def _get_pref_for(account):
+    return Preference.query.filter((Preference.account == account)).one_or_none()
+
+
+def _format(prefs):
+    return {"version": "v0", "preferences": prefs}
+
+
 def get():
-    res = Preference.query.filter((Preference.account == user["account"])).one_or_none()
-    doc = {"version": "v0", "prefs": res.prefs if res else None}
-    return doc, 200
+    res = _get_pref_for(user["account"])
+    return _format(res.prefs if res else None), 200
 
 
 def post(preference_set):
-    p = Preference(account=user["account"], prefs=preference_set["preferences"])
-    p.save()
-    return p.prefs, 202
+    existing = _get_pref_for(user["account"])
+    if not existing:
+        p = Preference(account=user["account"], prefs=preference_set["preferences"])
+        p.save()
+        return _format(p.prefs), 201
+    else:
+        existing.prefs = preference_set["preferences"]
+        existing.save()
+        return _format(existing.prefs), 200
